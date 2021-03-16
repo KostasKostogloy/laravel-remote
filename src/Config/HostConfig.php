9e6874d051283bb4217638dba1837b836f5848ad
Kostas Kostoglou <kostaskostogloy@outlook.com>
2021-03-16 17:53:00 +0200

    feat: php 7.4 compatibility

--- src/Config/HostConfig.php
+++ src/Config/HostConfig.php
@@ -4,11 +4,15 @@ namespace Spatie\Remote\Config;
 
 class HostConfig
 {
-    public function __construct(
-        public string $host,
-        public int $port,
-        public string $user,
-        public string $path,
-    ) {
+    public string $host;
+    public int $port;
+    public string $user;
+    public string $path;
+
+    public function __construct(string $host, int $port, string $user, string $path) {
+        $this->host = $host;
+        $this->port = $port;
+        $this->user = $user;
+        $this->path = $path;
     }
 }
